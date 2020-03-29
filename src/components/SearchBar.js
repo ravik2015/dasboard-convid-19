cafddcc7cf8a5aa1d93ee5e97c310e76ec88ee01
Ravi Kumar <ravi.kumar@smartdatainc.net>
2020-03-29 21:38:20 +0530

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, TextInput, Image} from 'react-native';
+
+import styles from '../utils/styles';
+
+const SearchBar = props => {
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <View style={styles.searchBar.container}>
+        <TextInput
+          style={styles.searchBar.inputStyle}
+          placeholder="Serach here..."
+          placeholderTextColor={styles.searchBar.placeholderColor}
+          onChangeText={props.onSearch}
+        />
+        <Image
+          source={require('../assets/search.png')}
+          style={styles.searchBar.logo}
+        />
+      </View>
+    </View>
+  );
+};
+
+export {SearchBar};
